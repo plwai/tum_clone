131c60cdb4ed37e2a0d559eefc31f8247f3306d2
plwai <pailee.wai@gmail.com>
2016-05-09 23:28:51 +0900

    Base front end structure -build architecture for website app -add static loader code for node_modules in app.js

---
+++ public/user-app/component/app.component.ts
@@ -0,0 +1,13 @@
+import {Component,
+        FORM_DIRECTIVES,
+        CORE_DIRECTIVES} from 'angular2/core';
+
+@Component({
+  selector: "my-app",
+  templateUrl: "user-app/template/app.component.html",
+  styleUrls: ["stylesheets/user-app/app.component.css"]
+})
+
+export class AppComponent{
+
+}
---
+++ public/user-app/main.ts
@@ -0,0 +1,4 @@
+import {AppComponent} from 'user-app/component/app.component.ts'
+import {bootstrap}    from 'angular2/platform/browser';
+
+bootstrap(AppComponent);
