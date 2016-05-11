fd009d5bcef093670ef2502270ca1c9ca8e703b0
plwai <pailee.wai@gmail.com>
2016-05-11 22:22:15 +0900

    basic search UI and logo

---
+++ public/user-app/component/search.component.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: "search-bar",
+  templateUrl: "user-app/template/search.component.html",
+  styleUrls: ["stylesheets/user-app/search.component.css"]
+})
+
+export class SearchComponent{
+
+}
